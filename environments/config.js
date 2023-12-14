0e40a94cae4b521a3786333a32a28a2676377baa
aaraam <aaramblkice@gmail.com>
2023-12-14 13:05:43 +0530

    feat: added dotenv + config

--- environments/config.js
+++ environments/config.js
@@ -1,4 +1,15 @@
 module.exports = {
-    testnet: {},
-    mainnet: {},
+    testnet: {
+        goerli: {},
+        sepolia: {},
+        mumbai: {},
+        binance: {},
+        avalanche: {},
+    },
+    mainnet: {
+        ethereum: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        polygon: {},
+        binance: {},
+        avalanche: {},
+    },
 };
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
 
 // middleware
 app.use(express.json());
